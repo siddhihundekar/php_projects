fb8712fd5eea79b413bef962760f2d2858f6c0d9
Siddhi Pravin Hundekar <hundekarsiddhi@gmail.com>
2025-03-21 14:47:56 +0530

    Array

---
+++ php_types_of_array/index.php
@@ -0,0 +1,19 @@
+<!-- indexed array -->
+<?php
+    // $array_A=array(1,2,3,4);                 
+    // print_r($array_A);
+
+
+    $array_A=[1,2.3,"string",true,Null];       
+    print_r($array_A);
+?>
+
+
+
+<!-- associative array -->
+<?php
+     $car=array("company"=>"Maruti","Model"=>"Breza","manufactured"=>2025);
+     print($car["company"]);
+     print_r($car);
+?>
+
