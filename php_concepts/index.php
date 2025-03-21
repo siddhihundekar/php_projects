16f0eed8c38b359bb2557e89e495f422cf35bee5
Siddhi Pravin Hundekar <hundekarsiddhi@gmail.com>
2025-03-21 14:51:31 +0530

    pi, min, max, abs, sqrt

---
+++ php_concepts/index.php
@@ -0,0 +1,80 @@
+<!-- pi function -->
+<?php
+    $pi=pi();
+    echo $pi;
+
+    echo "</br>";
+?>
+
+<!-- smallest number -->
+<?php
+    $min = min(4,3,5);
+    echo $min;
+
+    echo "</br>";
+
+    // $min = min["4","3","5","8","9"];
+    // echo $min;
+
+    // echo "</br>";
+?>
+
+<!-- largest number -->
+<?php
+    $max = max(4,3,5);
+    echo $max;
+
+    echo "</br>";
+
+    // $max = max[4,3,5,8,9];
+    // echo $max;
+
+    // echo "</br>";
+?>
+
+<!-- absolute number -->
+<?php
+    $a=10;
+    $b=-2;
+    $c=$a/$b;
+    $c=($c);
+    echo $c;
+
+    echo "</br>";
+
+    $a=10;
+    $b=-2;
+    $c=$a/$b;
+    $c=abs($c);
+    echo $c;
+
+    echo "</br>";
+
+    $a=10;
+    $b=2;
+    $c=$a/$b;
+    $c=abs($c);
+    echo $c;
+
+    echo "</br>";
+?>
+
+<!-- round number -->
+<?php
+    $c=8.666;
+    echo round($c,2);
+
+    echo "</br>";
+?>
+
+<!-- squareroot -->
+<?php
+    $a=100;
+    echo sqrt($a);
+
+    echo "</br>";
+?>
+
+
+
+
