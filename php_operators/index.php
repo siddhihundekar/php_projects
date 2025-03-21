89ee1982fa5605e1e7024519f2c29f6a11b3db30
Siddhi Pravin Hundekar <hundekarsiddhi@gmail.com>
2025-03-21 14:48:44 +0530

    Operators

---
+++ php_operators/index.php
@@ -0,0 +1,81 @@
+<!--Arithmethic Operators in php -->
+<?php
+
+   $a=10;
+   $b=2;
+
+   $c=$a+$b;        // addition
+   echo ($c);
+   echo "</br>";
+
+   $c=$a-$b;        // subtraction
+   echo ($c);
+   echo "</br>";
+
+   $c=$a*$b;        // multiplication
+   echo ($c);
+   echo "</br>";
+
+   $c=$a/$b;        // division
+   echo ($c);
+   echo "</br>";
+
+   $c=$a%$b;        // modulus means reminder
+   echo ($c);
+   echo "</br>";
+
+   $c=$a**$b;       // exponential
+   echo ($c);
+   echo "</br>";
+
+?>
+
+<br>
+<br>
+
+<!-- Relational Operators in php -->
+<?php
+
+   $a=10;
+   $b=2;
+
+   $c = $a > $b;               // greater than
+   echo ($c);
+   echo "</br>";
+
+   $a=5;
+   $b=4;
+
+   $c = $a >= $b;             // greater than or equals to
+   echo ($c);
+   echo "</br>";
+
+   $a=6;
+   $b=4;
+
+   $c = $a < $b;             // less than
+   echo ($c);
+   echo "</br>";
+
+   $a=4;
+   $b=4;
+
+   $c = $a <= $b;            // less than or equals to
+   echo ($c);
+   echo "</br>";
+
+   $a=8;
+   $b=8;
+
+   $c = $a == $b;            // is equals to
+   echo ($c);
+   echo "</br>";
+
+   $a=5;
+   $b=4;
+
+   $c = $a != $b;            // is not equals to
+   echo ($c);
+   echo "</br>";
+
+?>
