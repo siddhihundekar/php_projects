53e17f61cd84f5a3d6981566379bc4ffbfe5819b
Siddhi Pravin Hundekar <hundekarsiddhi@gmail.com>
2025-03-21 14:49:19 +0530

    Add files via upload

---
+++ php_int_string_array/index.php
@@ -0,0 +1,140 @@
+<?php  
+    echo "Hii I am Siddhi";                        //echo is used to print
+?>
+
+<br>
+<br>
+
+<?php
+    $name = "Siddhi";
+    // $name = true;
+    // $name = 164;
+    // $name = 25.29;
+    // $name = false;
+    echo $name;
+
+?>
+
+<br>
+<br>
+<br>
+<br>
+
+<!-- string to array -->
+<?php
+    $val = "123";
+    var_dump(value:$val);
+    echo "\n";
+    $val = (array)$val;
+    var_dump(value:$val);
+?>
+
+<br>
+<br>
+
+<!-- string to int -->
+<?php 
+    $val = "123siddhi";
+    var_dump(value:$val);
+    echo "\n";
+    $val = (int)$val;
+    var_dump(value:$val);
+?>
+
+<br>
+<br>
+
+<!-- string to float -->
+<?php 
+    $val = "123.32siddhi";
+    var_dump(value:$val);
+    echo "\n";
+    $val = (float)$val;
+    var_dump(value:$val);
+?>
+
+<br>
+<br>
+<br>
+<br>
+
+<!-- int to array -->
+<?php 
+    $val = 123;
+    var_dump(value:$val);
+    echo "\n";
+    $val = (array)$val;
+    var_dump(value:$val);
+?>
+
+<br>
+<br>
+
+<!-- int to string -->
+<?php 
+    $val = 123;
+    var_dump(value:$val);
+    echo "\n";
+    $val = (string)$val;
+    var_dump(value:$val);
+?>
+
+<br>
+<br>
+
+<!-- int to float -->
+<?php 
+    $val = 123;
+    var_dump(value:$val);
+    echo "\n";
+    $val = (float)$val;
+    var_dump(value:$val);
+?>
+
+<br>
+<br>
+<br>
+<br>
+
+<!-- float to array -->
+<?php 
+    $val = 123.3625;
+    var_dump(value:$val);
+    echo "\n";
+    $val = (array)$val;
+    var_dump(value:$val);
+?>
+
+<br>
+<br>
+
+<!-- float to string -->
+<?php 
+    $val = 123.3625;
+    var_dump(value:$val);
+    echo "\n";
+    $val = (string)$val;
+    var_dump(value:$val);
+?>
+
+<br>
+<br>
+
+<!-- float to int -->
+<?php 
+    $val = 123.3625;
+    var_dump(value:$val);
+    echo "\n";
+    $val = (int)$val;
+    var_dump(value:$val);
+?>
+
+<br>
+<br>
+<br>
+<br>
+
+
+
+
+ 
